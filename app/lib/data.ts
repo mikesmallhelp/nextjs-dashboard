8840e7515c91e74e76b9ffed0ff69ab7bdd864a8
Mike <mikesmallhelp@gmail.com>
2024-09-15 11:48:20 +0300

    Latest invoices with suspense

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -30,12 +30,17 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    console.log('Fetching latest invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+      console.log('Data fetch completed after 5 seconds.');
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
