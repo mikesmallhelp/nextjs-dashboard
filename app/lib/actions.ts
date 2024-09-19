61fcca978f7c29582f91efa83f147f7c70789e82
Mike <mikesmallhelp@gmail.com>
2024-09-19 16:36:48 +0300

    Fix the eslint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -66,7 +66,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
-    } catch (error) {
+    } catch (error) { // eslint-disable-line @typescript-eslint/no-unused-vars
         // If a database error occurs, return a more specific error.
         return {
             message: 'Database Error: Failed to Create Invoice.',
@@ -111,7 +111,7 @@ export async function updateInvoice(
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
       `;
-    } catch (error) {
+    } catch (error) { // eslint-disable-line @typescript-eslint/no-unused-vars
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
 
@@ -124,7 +124,7 @@ export async function deleteInvoice(id: string) {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
         return { message: 'Deleted Invoice.' };
-    } catch (error) {
+    } catch (error) { // eslint-disable-line @typescript-eslint/no-unused-vars
         return { message: 'Database Error: Failed to Delete Invoice.' };
     }
 }
